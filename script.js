0be3d39783c9e72a903f33bd3b2e8dc1d449c3e3
Jun Siang <lohjunsiang26@gmail.com>
2025-10-31 20:41:34 +0800

    Modifications in design and content

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('scroll', () => {
 
   sections.forEach(sec => {
     const id = sec.getAttribute('id');
-    const link = document.querySelector(`nav a[href="#${id}"]`);
+    const link = document.querySselector(`nav a[href="#${id}"]`);
     if (scrollPos > sec.offsetTop && scrollPos < sec.offsetTop + sec.offsetHeight) {
       link.classList.add('active');
     } else {
